27b846b223734b5f2b0deeb886676590a37464d8
Skanda <skanda.narayana@in.bosch.com>
2022-04-21 12:27:51 +0530

    test change

--- receiver/processInput.cpp
+++ receiver/processInput.cpp
@@ -16,19 +16,39 @@ void readParamFromConsole(){
 }
 
 int getTemperatureMax(){
-    return *(std::max_element(temperatureInput.begin(), temperatureInput.end()));
+    int max = temperatureInput[0];
+    for(auto & elem : temperatureInput){
+        if(elem > max) max = elem;
+    }
+    return max;
+//    return *(std::max_element(temperatureInput.begin(), temperatureInput.end()));
 }
 
 int getTemperatureMin(){
-    return *(std::min_element(temperatureInput.begin(), temperatureInput.end()));
+    int tempMin = temperatureInput[0];
+    for(auto & elem : temperatureInput){
+        if(elem < tempMin) tempMin = elem;
+    }
+    return tempMin;
+//    return *(std::min_element(temperatureInput.begin(), temperatureInput.end()));
 }
 
 int getVoltageMax(){
-    return *(std::max_element(voltageInput.begin(), voltageInput.end()));
+    int voltMax = voltageInput[0];
+    for(auto & elem : voltageInput){
+        if(elem > voltMax) voltMax = elem;
+    }
+    return voltMax;
+//    return *(std::max_element(voltageInput.begin(), voltageInput.end()));
 }
 
 int getVoltageMin(){
-    return *(std::min_element(voltageInput.begin(), voltageInput.end()));
+    int voltMin = voltageInput[0];
+    for(auto & elem : voltageInput){
+        if(elem < voltMin) voltMin = elem;
+    }
+    return voltMin;
+//    return *(std::min_element(voltageInput.begin(), voltageInput.end()));
 }
 
 int getSimpleMovingAverageTemperature(){
